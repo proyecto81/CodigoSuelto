99bf1ab6bd5587e387516963e3a2245dc040f0e6
Proyecto81 <proyecto81@gmail.com>
2015-03-05 19:27:05 -0300

    Base de Grilla
    Prototipo sin ajustes.

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+
+<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<!-- Responsive and mobile friendly stuff -->
+<meta name="HandheldFriendly" content="True">
+<meta name="MobileOptimized" content="320">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Menu -- | Menu Creado con http://cssmenumaker.com -->
+<link rel="stylesheet" type="text/css" href="Template/cssmenu/menu_source/styles.css"/>
+
+<!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements and feature detects -->
+<script src="js/modernizr.custom.js"></script>
+<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+
+<!-- Stylesheets -->
+<link rel="stylesheet" type="text/css" href="Template/css/normalize.css"/>
+<link rel="stylesheet" type="text/css" href="Template/css/basics.css"/>
+<link rel="stylesheet" type="text/css" href="Template/css/gs12_rs.css"/>
+<link rel="stylesheet" type="text/css" href="Template/css/styles.css"/>
+<title>Backend</title>
+</head>
+<body>
+<header>
+  <div id="headcont" class="bg_green">
+    <nav id='cssmenu'>
+      <ul>
+        <li><a href='#'><span>Inicio</span></a></li>
+        <li class='has-sub'><a href='#'><span>Opcion 1</span></a>
+          <ul>
+            <li><a href='#'><span>Sub Opcion A</span></a></li>
+            <li><a href='#'><span>Sub Opcion B</span></a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+<section id="contenido">
+  <div id="pagina">
+    <div class="cbody">
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1 </div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="clear"></div>
+      <div class="cnt_2 bg_blue">1</div>
+      <div class="cnt_2 bg_blue">1</div>
+      <div class="cnt_2 bg_blue">1</div>
+      <div class="cnt_2 bg_blue">1</div>
+      <div class="cnt_2 bg_blue">1</div>
+      <div class="cnt_2 bg_blue">1</div>
+      <div class="clear"></div>
+      <div class="cnt_3 bg_blue">1</div>
+      <div class="cnt_3 bg_blue">1</div>
+      <div class="cnt_3 bg_blue">1</div>
+      <div class="cnt_3 bg_blue">1</div>
+      <div class="clear"></div>
+      <div class="cnt_4 bg_blue"> 1 </div>
+      <div class="cnt_4 bg_blue">1</div>
+      <div class="cnt_4 bg_blue">1</div>
+      <div class="clear"></div>
+      <div class="cnt_5 bg_blue">1</div>
+      <div class="cnt_5 bg_blue">1</div>
+      <div class="cnt_2 bg_blue">1</div>
+      <div class="clear"></div>
+      <div class="cnt_6 bg_blue">1</div>
+      <div class="cnt_6 bg_blue">1</div>
+      <div class="clear"></div>
+      <div class="cnt_7 bg_blue"> 1 </div>
+      <div class="cnt_5 bg_blue">1</div>
+      <div class="clear"></div>
+      <div class="cnt_8 bg_blue">1</div>
+      <div class="cnt_4 bg_blue">1</div>
+      <div class="clear"></div>
+      <div class="cnt_9 bg_blue">1</div>
+      <div class="cnt_3 bg_blue"> 1 </div>
+      <div class="clear"></div>
+      <div class="cnt_10 bg_blue">1</div>
+      <div class="cnt_2 bg_blue">1</div>
+      <div class="clear"></div>
+      <div class="cnt_11 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="clear"></div>
+      <div class="cnt_12 bg_blue">1</div>
+      
+      <!-- Con Box de Titulos -->
+      <div class="separador"></div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_3 bg_blue">1</div>
+      <div class="cnt_5 bg_blue">1</div>
+      <div class="cnt_1 bg_blue">1</div>
+      <div class="cnt_2 bg_blue">1</div>
+      
+      <!-- Con Cols interioresde Titulos -->
+      <div class="separador"></div>
+      <div class="cnt_5 bg_pure_yellow">
+        <div class="cl_2 bg_blue">2</div>
+        <div class="cl_3 bg_blue">3</div>
+      </div>
+      <div class="cnt_4 bg_pure_yellow">
+        <div class="cl_4 bg_blue">2</div>
+      </div>
+      <div class="cnt_3 bg_pure_yellow">
+        <div class="cl_1 bg_blue">1</div>
+        <div class="cl_1 bg_blue">1</div>
+        <div class="cl_1 bg_blue">1</div>
+      </div>
+    </div>
+  </div>
+</section>
+<footer> </footer>
+</body>
+</html>
