849a45a3acfa23ec683cf77c1b7b3b672ba224e9
proyecto81 <proyecto81@gmail.com>
2018-05-14 09:47:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
+    public function nuevo() {
         $distribuidor = $this->Distribuidores->newEntity();
         if ($this->request->is('post')) {
             $post_data = $this->request->data();
             $distribuidor = $this->Distribuidores->patchEntity($distribuidor, $post_data, [
                 'associated' => ['Cuentas', 'Cuentas.Users']]);
-            
+
             // Datos de Distribuidor
             $distribuidor->estado = 1;
 
@@ -12,18 +13,20 @@
             $distribuidor->cuenta->email = $post_data['email'];
             $distribuidor->cuenta->tipo_cuenta_id = 1;
             $distribuidor->cuenta->estado = 1;
-            
+
             // Datos de User
             $user = $this->Users->newEntity();
             $user->tipo_user_id = $post_data['tipo_user'];
             $user->username = $post_data['email'];
             $user->email = $post_data['email'];
             $user->password = $post_data['clave_cuenta'];
-            $distribuidor->cuenta->user = $user;
+            $distribuidor->cuenta->users = [];
+            array_push($distribuidor->cuenta->users, $user);
             
+            // Habilitamos saves
             $distribuidor->dirty('cuenta', true);
-            //$distribuidor->cuenta->dirty('user', true);
-            
+            $distribuidor->cuenta->dirty('user', true);
+
             if ($this->Distribuidores->save($distribuidor)) {
                 $this->Flash->success(__('El distribuidor fue creado con exito.'));
             } else {
